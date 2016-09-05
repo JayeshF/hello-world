8d4f47fad6cfb8c557c8a04d9c4b1848b70a16de
JayeshF <jayeshfulzele@gmail.com>
2016-09-05 10:54:41 -0500

    hello-world
    updated hello world python program

---
+++ helloworld.py
@@ -0,0 +1,17 @@
+#Python 2.7.12 (v2.7.12:d33e0cf91556, Jun 27 2016, 15:19:22) [MSC v.1500 32 bit (Intel)] on win32
+#Type "copyright", "credits" or "license()" for more information.
+
+import sys
+import time
+
+def main():
+        print 'Hello world'
+        time.sleep(5)
+        exit()        
+
+if __name__ == '__main__':
+        main()
+        
+        
+
+	
